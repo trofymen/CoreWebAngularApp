e09e32da3e94a29f018a73ff1d8b19336521d49a
Pavlo Trofymenko <ptrofymen@gmail.com>
2020-06-04 15:58:43 +0300

    added migrations

---
+++ CoreWebAngularApplication/Migrations/20200602163419_migration1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreWebAngularApplication.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Company = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Login", "Password", "Role" },
+                values: new object[,]
+                {
+                    { 1, "admin@gmail.com", "12345", "admin" },
+                    { 2, "qwerty@gmail.com", "55555", "user" },
+                    { 3, "qwerty2@gmail.com", "77777", "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Company", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Apple", "iPhone X", 79900 },
+                    { 2, "Samsung", "Galaxy S8", 49900 },
+                    { 3, "Google", "Pixel 2", 52900 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
